cfe4873c8028fffd8586c8e84ad54216a798822c
rickyfriady <friadi.ricki@gmail.com>
2023-04-15 22:34:38 +0700

    make principle design components

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,4 @@
+export default function Button({ props, children }: any) {
+  const { variants } = props;
+  return <button>{children}</button>;
+}
---
+++ src/components/molecules/SearchItems.tsx
@@ -0,0 +1,10 @@
+import Button from '../atoms/Button';
+
+export default function SearchItems() {
+  return (
+    <div>
+      <h1>SearchItems</h1>
+      <Button variants="blue">Search</Button>
+    </div>
+  );
+}
